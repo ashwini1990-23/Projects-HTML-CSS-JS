3f58c329d682337c8cff7af005d615bda4efee9d
Ashwini Patil <ashwini.aski@gmail.com>
2025-05-13 15:30:55 -0400

    Adding reference code for Advanced DOM practice

--- 5-Bankist-WebSite/script.js
+++ 5-Bankist-WebSite/script.js
@@ -33,3 +33,51 @@ document.addEventListener('keydown', function (e) {
 });
 
 
+////////////////////////////////
+// **** Selecting Elements ****
+// Selecting the entire document of a web page
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header'); // returns first element that matches the selector(class '.header')
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button'); // returns live HTMLCollection
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn')); // returns live HTMLCollection
+
+// **** Creating and Inserting Elements ****
+// .insertAdjacentHTML(position, html) : inserts HTML code into a specified position
+// 4 position values:1) afterbegin  2) afterend: after the element 3)beforebegin:before the element 4)beforeend
+
+// Creating element
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close--cookie">Got it!</button>';
+//Inserting element
+// header.prepend(message);
+header.append(message);
+//header.append(message.cloneNode(true));
+
+//header.before(message.cloneNode(true));
+//header.after(message);
+
+// **** Delete Elements ****
+const cookieBtn = document.querySelector('.btn--close--cookie');
+cookieBtn.addEventListener('click', function () {
+  //message.parentElement.removeChild(message);
+  message.remove();
+})
+
+// Styles
+message.style.backgroundColor = '#37383d'; // to set the style
+message.style.width = '103.5%';
+
+console.log(message.style.height); // get or read the style
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
