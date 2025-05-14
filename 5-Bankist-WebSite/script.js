5a5230510d871ec12861b521d754bcda72b43473
Ashwini Patil <ashwini.aski@gmail.com>
2025-05-14 15:54:55 -0400

    Adding Styles,Classes,Attributes and Smooth scrolling code

--- 5-Bankist-WebSite/script.js
+++ 5-Bankist-WebSite/script.js
@@ -32,7 +32,39 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+// Smoothly Scrolling
+// Way-1
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.getElementById('section--1');
+const section1 = document.querySelector('#section--1');
 
+btnScrollTo.addEventListener('click', function (e) {
+  //Co-ordinates
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+  //Window Scrolling
+  console.log('Current scroll X/Y', window.scrollX, window.scrollY);
+  // Viewport dimensions
+  console.log('Height and Width of view port: ', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+  // Scrolling
+  /*
+  // window.scrollTo(s1coords.left + window.scrollX, s1coords.top + window.scrollY); OR
+  
+    window.scrollTo({
+      left: s1coords.left + window.scrollX,
+      top: s1coords.top + window.scrollY,
+      behavior: 'smooth',
+    });*/
+
+  // Way-2 : works for modern browsers
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+/*
 ////////////////////////////////
 // **** Selecting Elements ****
 // Selecting the entire document of a web page
@@ -52,15 +84,15 @@ console.log(document.getElementsByClassName('btn')); // returns live HTMLCollect
 
 // **** Creating and Inserting Elements ****
 // .insertAdjacentHTML(position, html) : inserts HTML code into a specified position
-// 4 position values:1) afterbegin  2) afterend: after the element 3)beforebegin:before the element 4)beforeend
+// 4 position values:1) afterbegin:firstchild 2) afterend: after the element 3)beforebegin:before the element 4)beforeend:lastchild
 
 // Creating element
-const message = document.createElement('div');
+const message = document.createElement('div'); // returns DOM element
 message.classList.add('cookie-message');
 message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close--cookie">Got it!</button>';
 //Inserting element
-// header.prepend(message);
-header.append(message);
+// header.prepend(message); // insert element as first child of other element
+header.append(message); // insert element as last child of other element
 //header.append(message.cloneNode(true));
 
 //header.before(message.cloneNode(true));
@@ -73,11 +105,54 @@ cookieBtn.addEventListener('click', function () {
   message.remove();
 })
 
-// Styles
+// **** Styles ****
+// Set style on an element
 message.style.backgroundColor = '#37383d'; // to set the style
 message.style.width = '103.5%';
 
 console.log(message.style.height); // get or read the style
 console.log(message.style.backgroundColor);
 
+// To read or get styles from css files
 console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height); // returns string
+// console.log(Number.parseFloat(getComputedStyle(message).height));
+message.style.height = Number.parseFloat(getComputedStyle(message).height) + 20 + 'px';
+console.log(getComputedStyle(message).height);
+// CSS Custom properties or CSS Variables
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// **** Attributes ****
+// reading statndard html element attributes from JS
+const logo = document.querySelector('.nav__logo');
+logo.alt = 'Beautiful minimalist logo';
+// absolute url of src attribute
+console.log(logo.src);
+console.log(logo.alt);
+console.log(logo.className);
+
+// reading Non-standard or custom attributes of html element from JS using getAttribute() function
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+// relative url of src attribute
+console.log(logo.getAttribute('src'));
+// same is true for links(<a href=''></a>)
+const link = document.querySelector('.twitter-link');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+const link2 = document.querySelector('.nav__link--btn');
+console.log(link2.href);
+console.log(link2.getAttribute('href'));
+
+// Data Attributes : are stored in dataset object
+console.log(logo.dataset.versionNumber);
+
+// **** Classes ****
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+*/
